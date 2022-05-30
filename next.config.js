17416befd0f1161fc1ce99cf6055499e92682fdf
Elyas <lost_ring123@yahoo.com>
2022-05-30 10:49:55 +0430

    fix(main) : changed image name and the path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
+// const runtimeCaching = require("next-pwa/cache");
 
 const nextConfig = withPWA({
   reactStrictMode: true,
@@ -8,9 +8,9 @@ const nextConfig = withPWA({
     dest: "public",
     register: true,
     skipWaiting: true,
-    runtimeCaching,
+    // runtimeCaching,
     fallbacks: {
-      image: "/static/image/no-internet-dinosaur.jpg",
+      image: "/static/image/fallback.jpg",
       // document: '/other-offline',  // if you want to fallback to a custom page other than /_offline
       // font: '/static/font/fallback.woff2',
       // audio: ...,
